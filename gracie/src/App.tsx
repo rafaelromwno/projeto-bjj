110c116c3eb1435e3ea6c5e773b3dd9ee54df234
Rafael Romano Silva <romanosilvarafael@gmail.com>
2025-04-19 16:28:38 -0300

    feat: implementing react router

--- gracie/src/App.tsx
+++ gracie/src/App.tsx
@@ -1,9 +1,30 @@
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Index from "./pages/Index";
+import NotFound from "./pages/NotFound";
 
+const queryClient = new QueryClient();
 
-export default function App() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
-}
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
+      <BrowserRouter>
+        <Routes>
+
+          <Route path="/" element={<Index />} />
+
+          {/* captures improper or non-existent route "*" */}
+          <Route path="*" element={<NotFound />} />
+          
+        </Routes>
+      </BrowserRouter>
+    </TooltipProvider>
+  </QueryClientProvider>
+);
+
+export default App;
